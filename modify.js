9bfc7af9b605835a5af12897a3a0f65e710a84fe
Athurg Feng <athurg@gooth.org>
2017-08-17 10:30:20 +0800

    修正函数封装错误的问题，优化函数、变量命名

--- modify.js
+++ modify.js
@@ -1,5 +1,5 @@
 //更新背景色，当日入账金额大于0，就显示为绿色的
-function fix_calendar() {
+function fixCalendar() {
 	$("div.nameMoney span.money").each(function(){
 		//获取当日入账金额，如果为0则跳过
 		if (parseFloat($(this).text()) == 0) {
@@ -20,26 +20,34 @@ function mutationCallback(mutations, observer) {
 			return
 		}
 
-		fix_calendar();
+		fixCalendar();
 	});
+}
+
+//启动“周期帐”日历视图的观察器
+function loadCalendarObserver() {
+	//对id为calendar的table启动观察器
+	observer = new MutationObserver(mutationCallback);
+	table = document.querySelector('table#calendar');
+	if (table==null) {
+		return;
+	}
+	observer.observe(table, {childList: true, subtree: true});
 }
 
 //复制一个“周期帐”的导航项目，并放在顶层
 function duplicateBillNavItem() {
-	//对id为calendar的table启动观察器
-	var observer = new MutationObserver(mutationCallback);
-	observer.observe(document.querySelector('table#calendar'), {childList: true, subtree: true});
-
 	//找到被隐藏的“周期帐”导航链接
 	archor = $("a.g-top-bill-g");
 
 	//克隆一个“记账”导航项，修改链接地址和链接信息
-	var nav_li = $("li.l-tally").first().clone();
-	$(nav_li).find("a").html(archor.html());
-	$(nav_li).find("a").attr("href", archor.attr("href"));
+	var navLi = $("li.l-tally").first().clone();
+	$(navLi).find("a").html(archor.html());
+	$(navLi).find("a").attr("href", archor.attr("href"));
 
 	//将克隆的导航项放在“记账”导航项目前面（CSS样式匹配后界面显示为后面）
-	$("li.l-tally").before(nav_li);
+	$("li.l-tally").before(navLi);
 }
 
+loadCalendarObserver();
 duplicateBillNavItem();
