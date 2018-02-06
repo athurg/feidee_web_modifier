5b3c0159c884ed9e3dc3943fd04dcfee67153f7d
Athurg Feng <athurg@gooth.org>
2018-02-06 12:49:30 +0800

    修改缩进样式，用双空格取代制表符

--- feidee_web_modifier.safariextension/modify.js
+++ feidee_web_modifier.safariextension/modify.js
@@ -1,60 +1,60 @@
 /******************** 以下为函数定义 *******************/
 //“周期帐”页面背景色更新回调
 function calendarBackgroundUpdateCallback(mutations, observer) {
-	mutations.forEach(function(mutation) {
-		var spans=null;
-		if (mutation.target.tagName=="TBODY") {
-			//整月刷新
-			spans = mutation.target.querySelectorAll("span.money");
-		} else if (mutation.target.tagName=="SPAN" && mutation.target.classList.contains("money")) {
-			//单个日期刷新
-			spans = [mutation.target];
-		}
+  mutations.forEach(function(mutation) {
+    var spans=null;
+    if (mutation.target.tagName=="TBODY") {
+      //整月刷新
+      spans = mutation.target.querySelectorAll("span.money");
+    } else if (mutation.target.tagName=="SPAN" && mutation.target.classList.contains("money")) {
+      //单个日期刷新
+      spans = [mutation.target];
+    }
 
-		if (spans==null || spans.length==0) {
-			return;
-		}
+    if (spans==null || spans.length==0) {
+      return;
+    }
 
-		for (i=0; i<spans.length; i ++ ) {
-			span = spans[i];
-			//金额为0表示未入账，不作修改
-			if (parseFloat(span.innerText)==0) {
-				continue;
-			}
+    for (i=0; i<spans.length; i ++ ) {
+      span = spans[i];
+      //金额为0表示未入账，不作修改
+      if (parseFloat(span.innerText)==0) {
+        continue;
+      }
 
-			//如果金额为0但不包含old样式，表示已入账或者尚未到达日期，这个时候不应该包含part样式
-			if (!span.parentNode.parentNode.classList.contains("old")) {
-				span.parentNode.parentNode.classList.remove("part");
-				continue;
-			}
+      //如果金额为0但不包含old样式，表示已入账或者尚未到达日期，这个时候不应该包含part样式
+      if (!span.parentNode.parentNode.classList.contains("old")) {
+        span.parentNode.parentNode.classList.remove("part");
+        continue;
+      }
 
-			//如果金额为0且包含old样式，表示已部分入账。应该修改old样式为part样式
-			span.parentNode.parentNode.classList.remove("old");
-			span.parentNode.parentNode.classList.add("part");
-		}
-	});
+      //如果金额为0且包含old样式，表示已部分入账。应该修改old样式为part样式
+      span.parentNode.parentNode.classList.remove("old");
+      span.parentNode.parentNode.classList.add("part");
+    }
+  });
 }
 
 //复制一个“周期帐”的导航项目，并放在顶层
 function duplicateBillNavItem() {
-	//找到被隐藏的“周期帐”导航链接
-	archor = $("a.g-top-bill-g");
+  //找到被隐藏的“周期帐”导航链接
+  archor = $("a.g-top-bill-g");
 
-	//克隆一个“记账”导航项，修改链接地址和链接信息
-	var navLi = $("li.l-tally").first().clone();
-	$(navLi).find("a").html(archor.html());
-	$(navLi).find("a").attr("href", archor.attr("href"));
-	$(navLi).addClass("l-bill");
-	$(navLi).removeClass("l-tally");
+  //克隆一个“记账”导航项，修改链接地址和链接信息
+  var navLi = $("li.l-tally").first().clone();
+  $(navLi).find("a").html(archor.html());
+  $(navLi).find("a").attr("href", archor.attr("href"));
+  $(navLi).addClass("l-bill");
+  $(navLi).removeClass("l-tally");
 
-	//切换导航栏是否是选中状态
-	$(navLi).removeClass("select");
-	if (window.location.pathname == "/bill/index.do" || window.location.pathname == "/money/bill/index.do") {
-		$(navLi).addClass("select");
-	}
+  //切换导航栏是否是选中状态
+  $(navLi).removeClass("select");
+  if (window.location.pathname == "/bill/index.do" || window.location.pathname == "/money/bill/index.do") {
+    $(navLi).addClass("select");
+  }
 
-	//将克隆的导航项放在“记账”导航项目前面（CSS样式匹配后界面显示为后面）
-	$("li.l-tally").before(navLi);
+  //将克隆的导航项放在“记账”导航项目前面（CSS样式匹配后界面显示为后面）
+  $("li.l-tally").before(navLi);
 }
 
 //tip列表观察器回调
@@ -91,41 +91,41 @@ function calendarTipMutationCallback(mutations, observer) {
 /******************** 以下为执行代码 *******************/
 //调整“周期帐”页面部分入账日期的样式
 try {
-	$('div.tipColor ul').append('<li><a class="backgroundGreen" style="background-color:#898922"></a><li style="padding-top: 6px;">部分入账</li>');
-	target = document.querySelector('table#calendar');
-	if (target!=null) {
-		new MutationObserver(calendarBackgroundUpdateCallback).observe(target, {childList: true, subtree: true});
-	}
+  $('div.tipColor ul').append('<li><a class="backgroundGreen" style="background-color:#898922"></a><li style="padding-top: 6px;">部分入账</li>');
+  target = document.querySelector('table#calendar');
+  if (target!=null) {
+    new MutationObserver(calendarBackgroundUpdateCallback).observe(target, {childList: true, subtree: true});
+  }
 }catch(e){
-	console.log(e);
+  console.log(e);
 }
 
 //添加／更新“周期帐”顶级导航项
 try{
-	duplicateBillNavItem();
+  duplicateBillNavItem();
 }catch(e){
-	console.log(e);
+  console.log(e);
 }
 
 //添加／更新“周期帐”入账弹出页面的金额汇总信息
 try{
-	target = document.querySelector('dl#tip_list_dl');
-	if (target!=null) {
-		new MutationObserver(calendarTipMutationCallback).observe(target, {childList: true});
-	}
+  target = document.querySelector('dl#tip_list_dl');
+  if (target!=null) {
+    new MutationObserver(calendarTipMutationCallback).observe(target, {childList: true});
+  }
 }catch(e){
-	console.log(e);
+  console.log(e);
 }
 
 //给“记账”页面条目清单中的转账条目增加“转”的文本标志
 try{
-	target = document.querySelector('div#list');
-	if (target!=null) {
-		callback = function(){
-			$("span.typename2").html('(转)');
-		};
-		new MutationObserver(callback).observe(target, {childList: true});
-	}
+  target = document.querySelector('div#list');
+  if (target!=null) {
+    callback = function(){
+      $("span.typename2").html('(转)');
+    };
+    new MutationObserver(callback).observe(target, {childList: true});
+  }
 }catch(e){
-	console.log(e);
+  console.log(e);
 }
